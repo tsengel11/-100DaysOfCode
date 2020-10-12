d9ee4c44e2838a82352c40ecba318fa38536eacf
Bayartsengel Baatarkhuyag <bayartsengelt@gmail.com>
2020-10-12 22:39:48 +1000

    Created main components

---
+++ src/component/dayList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Daylist extends React.Component {
+  render() {
+    return <div>Day list Component</div>;
+  }
+}
+
+export default Daylist;
---
+++ src/component/header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return <div>My 100 days of cofing challange</div>;
+  }
+}
+
+export default Header;
